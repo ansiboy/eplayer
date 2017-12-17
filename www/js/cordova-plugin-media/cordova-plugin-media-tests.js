69a14ea9e2813637a68f32f50f2b515a5867cbbb
maishu <ansiboy@163.com>
2017-12-17 11:00:11 +0800

    update

--- www/js/cordova-plugin-media/cordova-plugin-media-tests.js
+++
@@ -1,5 +0,0 @@
-// Media and Media Capture
-//----------------------------------------------------------------------
-var media = new Media('', () => { console.log('Media opened'); }, (err) => { alert('Error: ' + err.code); });
-media.play();
-media.setVolume(10);
