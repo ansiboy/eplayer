e3b93713768152a90019ba3ad47e8476dfd18c34
maishu-PC\maishu <ansiboy@163.com>
2018-03-05 10:44:04 +0800

    fix 开机无法启动的 BUG

--- platforms/android/src/io/cordova/hellocordova/BootReceiver.java
+++ platforms/android/src/io/cordova/hellocordova/BootReceiver.java
@@ -4,12 +4,13 @@ import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class BootReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
             Intent intent2 = new Intent(context, MainActivity.class);
-            intent2.setFlags(0);
+            intent2.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(intent2);
         }
     }
